b232bc75c1b1510aade90fd60a58701983a83023
ozturkemre58 <emreozturk.5874@gmail.com>
2024-01-27 23:10:33 +0300

    first commit

--- webSocketExample/ViewController.swift
+++ webSocketExample/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, URLSessionWebSocketDelegate {
+    
+    
+    private var webSocket: URLSessionWebSocketTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        let url = URL(string: "wss://socketsbay.com/wss/v2/1/demo/")
+        webSocket = session.webSocketTask(with: url!)
+        webSocket?.resume()
     }
-
-
+    
+    func ping() {
+        webSocket?.sendPing(pongReceiveHandler: { error in
+            if let error = error {
+                print("Ping Error \(error)")
+            }
+        })
+    }
+    
+    func close() {
+        webSocket?.cancel(with: .goingAway, reason: "demo ended".data(using: .utf8))
+    }
+    
+    func send() {
+        DispatchQueue.global().asyncAfter(deadline: .now()+1) {
+            self.send()
+            self.webSocket?.send(.string("Send new message: \(Int.random(in: 0...1000))"), completionHandler: { error in
+                if let error = error {
+                    print("Send error: \(error)")
+                }
+            })
+        }
+    }
+    
+    func receive() {
+        webSocket?.receive(completionHandler: { [weak self] result in
+            switch result {
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    print("Response Data : \(data)")
+                case .string(let message):
+                    print("Response message : \(message)")
+                @unknown default:
+                    break
+                }
+            case .failure(let error):
+                print("Recieve error \(error.localizedDescription)")
+            }
+            
+            self?.receive()
+        })
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        print("Did connect  socket")
+        ping()
+        receive()
+        send()    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        print("Did close connection with reason")
+    }
 }
 
